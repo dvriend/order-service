7ee2225ce5bb1b9c348948b06d692aa037fbeba1
David Vriend <dvriend2002@yahoo.com>
2023-05-04 06:27:42 -0500

    Thru p. 278, Listing 8.22, section 8.3

--- src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
+++ src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
@@ -1,5 +1,7 @@
 package com.polarbookshop.orderservice.order.domain;
 
+import com.polarbookshop.orderservice.book.Book;
+import com.polarbookshop.orderservice.book.BookClient;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -7,10 +9,12 @@ import reactor.core.publisher.Mono;
 @Service
 public class OrderService {
 
+	private final BookClient bookClient;
     private final OrderRepository orderRepository;
 
-    public OrderService(OrderRepository orderRepository) {
-        this.orderRepository = orderRepository;
+    public OrderService(BookClient bookClient, OrderRepository orderRepository) {
+		this.bookClient = bookClient;
+		this.orderRepository = orderRepository;
     }
 
     public Flux<Order> getAllOrders() {
@@ -18,10 +22,16 @@ public class OrderService {
     }
 
 	public Mono<Order> submitOrder(String isbn, int quantity) {
-		return Mono.just(buildRejectedOrder(isbn, quantity))
+		return bookClient.getBookByIsbn(isbn)
+				.map(book -> buildAcceptedOrder(book, quantity))
+				.defaultIfEmpty(buildRejectedOrder(isbn, quantity))
 				.flatMap(orderRepository::save);
 	}
 
+	public static Order buildAcceptedOrder(Book book, int quantity) {
+		return Order.of(book.isbn(), book.title() + " - " + book.author(),
+				book.price(), quantity, OrderStatus.ACCEPTED);
+	}
 	public static Order buildRejectedOrder(
 			String bookIsbn, int quantity
 	) {
